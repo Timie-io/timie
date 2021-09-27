108bf1dac12ce237b59273ba6161508272c832da
Merfrei <contact@merfrei.com>
2021-09-27 18:25:04 -0400

    Assignments: add task title as note if none is provided

--- src/assignments/assignments.service.ts
+++ src/assignments/assignments.service.ts
@@ -60,6 +60,9 @@ export class AssignmentsService {
     status: Status,
   ): Promise<Assignment> {
     const assignment = this.repository.create(data);
+    if (!assignment.note) {
+      assignment.note = task.title;
+    }
     assignment.task = task;
     assignment.user = user;
     assignment.creator = creator;
